4d3917c338d11a2e7db84478fd7cfa4a7f512b57
Kenneth <matovken@gmial.com>
2017-04-19 09:32:08 +0300

    adding api consumer

---
+++ api_consumer.py
@@ -0,0 +1,20 @@
+import http.client
+import json
+
+
+def get_quote():
+    headers = {'X-Mashape-Key': 'Z37r8KDorrmshToSBkYND18TileAp1rRYFFjsnYntDyPuYUx0u',
+               'Content-Type': 'application/x-www-form-urlencoded',
+               'Accept': 'application/json'}
+    domain = 'andruxnet-random-famous-quotes.p.mashape.com'
+    connection = http.client.HTTPSConnection(domain)
+    connection.request('POST', "/?cat=movies", headers=headers)
+    response = connection.getresponse()
+    if 199 < response.status < 300:
+        data = json.loads(response.read().decode())
+        return "Quote: `{}` by {}".format(data['quote'], data['author'])
+    return "Oops... Something didn't work: {}({})".format(response.status, response.reason)
+
+
+if __name__ == "__main__":
+    print(get_quote())
